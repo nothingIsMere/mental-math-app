12027d598d9d0629bf08fb2a4eee2e6dfb06e50b
Joel <joel.knoll1@gmail.com>
2025-11-02 15:59:35 -0500

    Finer control over number of digits.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mental-math-v1";
+const CACHE_NAME = "mental-math-v2";
 const urlsToCache = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
