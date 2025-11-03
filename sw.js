b9979a1fbd38c6dcc27d8eb1858ebdb34c41ab72
Joel <joel.knoll1@gmail.com>
2025-11-02 20:11:16 -0500

    Eliminated original Submit button.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mental-math-v3";
+const CACHE_NAME = "mental-math-v4";
 const urlsToCache = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
